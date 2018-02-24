234727b036b4cc8cf67bb6cf45f95ea830f7a0e7
adrian <adrian-sw@aporter.org>
2018-02-24 16:18:52 -0500

    minor:  split into new method.

--- simulate.py
+++ simulate.py
@@ -31,6 +31,36 @@ def usage():
 def parse(data):
 	return float(data) if data != '' else None
 
+def oneSimulation(data, startDate, endDate, goalYears, startMoneyToTakeOut, doInvest, doInflation):
+	date=startDate
+	startCpi=data[date]['cpi']
+	#the only reason to track "shares" istead of "money" is you don't have to look at previous rows
+	shares=1/data[date]['sp500'] if doInvest else 1
+	good = False
+	while date < endDate:
+		moneyToTakeOut = startMoneyToTakeOut * (data[date]['cpi'] / startCpi if doInflation else 1)
+		
+		sharesToTakeOut = moneyToTakeOut / ( data[date]['sp500'] if doInvest else 1 )
+
+		shares -= sharesToTakeOut
+		
+		shares += data[date]['dividends'] / data[date]['sp500'] * shares if doInvest else 0
+		
+		#print(moneyToTakeOut, sharesToTakeOut, shares)
+		if shares < 0: break
+		date+=relativedelta(years=1)
+		years = (date - startDate).days / 365
+		if years >= goalYears:
+			good = True
+			break
+	
+	if not good and date == endDate:
+		return None
+	
+	print(startDate, 'good' if good else 'bad')
+	return good
+	
+
 def run(goalYears, percentTakeOut, doInvest, doInflation):
 	#convert to yearly amount:
 	startMoneyToTakeOut = percentTakeOut/100
@@ -40,7 +70,6 @@ def run(goalYears, percentTakeOut, doInvest, doInflation):
 	for date,sp500,dividends,earnings,interest,governmentBond,cpi in csv.reader(open('shiller.csv', 'r')):
 		if date.lower() == 'date': continue
 		date=datetime.strptime(date,'%Y')
-		print(sp500, dividends, earnings, interest, governmentBond, cpi)
 		data[date]={'sp500': parse(sp500), 'dividends': parse(dividends), 'earnings': parse(earnings), \
 				'interest': parse(interest), 'governmentBond': parse(governmentBond), 'cpi': parse(cpi)}
 	
@@ -49,33 +78,14 @@ def run(goalYears, percentTakeOut, doInvest, doInflation):
 	goodCount = 0
 	totalCount = 0
 	while startDate < endDate:
-		date=startDate
-		startCpi=data[date]['cpi']
-		#the only reason to track "shares" istead of "money" is you don't have to look at previous rows
-		shares=1/data[date]['sp500'] if doInvest else 1
-		good = False
-		while date < endDate:
-			moneyToTakeOut = startMoneyToTakeOut * (data[date]['cpi'] / startCpi if doInflation else 1)
-			
-			sharesToTakeOut = moneyToTakeOut / ( data[date]['sp500'] if doInvest else 1 )
+		good = oneSimulation(data, startDate, endDate, goalYears, startMoneyToTakeOut, doInvest, doInflation)
 
-			shares -= sharesToTakeOut
-			
-			shares += data[date]['dividends'] / data[date]['sp500'] * shares if doInvest else 0
-			
-			#print(moneyToTakeOut, sharesToTakeOut, shares)
-			if shares < 0: break
-			date+=relativedelta(years=1)
-			years = (date - startDate).days / 365
-			if years >= goalYears:
-				goodCount += 1
-				good = True
-				break
+		if good is None: break
+
+		if good:
+			goodCount += 1
 		
-		if not good and date == endDate:
-			break
 		totalCount += 1
-		print(startDate, shares, 'good' if good else 'bad')
 		
 		startDate+=relativedelta(years=1)
 	print('suceeded at keeping retirement money %.0f years %.0f%% of the simulations' % (goalYears, goodCount / totalCount * 100))
